b6eb8f96cd5ef3dcf6c283752c532dd95ce99f83
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-02-13 14:30:33 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Image source={require('./assets/icon.png')} style={{ width: 305, height: 159, margin: 10 }} /> {/* Imagem local */}
+      <Image source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }} style={{ width: 64, height: 64, margin: 10 }} /> {/* Imagem da web */}
     </View>
   );
 }
